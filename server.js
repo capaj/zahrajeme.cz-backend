8f3dd3c07e79092c22826ebe81acf66cc25bf873
Jiri Spac <capajj@gmail.com>
2016-06-22 18:20:32 +0200

    authenticating connections on facebook graph API implemented

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('koa')()
 // const router = require('koa-router')
 const config = require('config')
 const RPC = require('socket.io-rpc')
-
+const request = require('request-promise')
 const server = require('http').Server(app.callback())
 
 const sioRPCServer = RPC(server)
@@ -10,6 +10,13 @@ const sioRPCServer = RPC(server)
 sioRPCServer.expose({
   hello: function () {
     console.log('hello')
+  },
+  auth: function* (token) {
+    const res = yield request('https://graph.facebook.com/me?access_token=' + token + '&fields=id,name,email,gender,installed,verified,location,hometown')
+    this.fbUser = JSON.parse(res)
+  },
+  getUser: function* () {
+    return this.fbUser
   }
 })
 
---
+++ test/auth.spec.js
@@ -0,0 +1,18 @@
+import test from 'ava'
+import rpcClient from 'socket.io-rpc-client'
+import config from '../config/default'
+
+const backend = rpcClient('http://localhost:' + config.port)
+
+test('auth', async t => {
+  // open graph test user's token
+  await backend('auth')("EAAEbR0ZAGqQcBAMQgaShhg1KdC190RPfmZCGwaRq6mZBY1YgcLxtu4n0oHH1ZBIe2jrMp09cN67XZASlA9lEQLGbV2LXR5vMqZBie46hi4LGbltmvwPmMutXcAhz8ZBNnX3Anl3gezKmuIVfriPDBfj178LThJhU4IyaC9imoE8asdqmVmuHZCsg")
+  const usr = await backend('getUser')()
+  t.deepEqual(usr, {
+    "id": "1709935319273719",
+    "name": "Open Graph Test User",
+    "gender": "male",
+    "installed": true,
+    "verified": false
+  })
+})
